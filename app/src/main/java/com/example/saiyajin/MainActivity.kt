3927695bbb02800224137b111c9bff780c15c9b8
daniela fuquene gallego <daniela.fuqueneg@perficient.com>
2023-05-15 20:50:01 -0500

    new project

---
+++ app/src/main/java/com/example/saiyajin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.saiyajin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+* Request de postman para este proyecto
+* https://www.getpostman.com/collections/f3cdc1abb737c0f0dc1c
+* */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
